93f4d9663e3e2bca0e9bac931e645c89ff984699
DESKTOP-GRNA5IG\于永星 <yuyongxing@pxene.com>
2021-11-12 00:21:34 +0800

    更新

--- 自动生成页面骨架/index.js
+++ 自动生成页面骨架/index.js
@@ -2,16 +2,16 @@
  * @Author: yuyongxing
  * @Date: 2021-11-10 19:00:21
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-11-11 19:51:57
+ * @LastEditTime: 2021-11-11 23:51:45
  * @Description: 
  */
 // https://github.com/famanoder/dps
 // https://blog.csdn.net/zz_jesse/article/details/116452550
 // dom元素过滤成骨架屏
 const puppeteer = require('puppeteer');
-const {getSkeletonHtml} = require('./util.js')
+const { getSkeletonHtml, getDomByScript } = require('./util.js')
 
-async function openPage(){
+async function openPage() {
   const option = {
     url: "http://landing.powerxene.com/static/test/index.html",
     device: "iPhone 6"
@@ -23,24 +23,24 @@ async function openPage(){
   // 设定打开页面的设备
   await page.emulate(puppeteer.devices[option.device])
   // 打开的页面地址
-  await page.goto(option.url,{waitUntil:"networkidle0"});
+  await page.goto(option.url, { waitUntil: "networkidle0" });
 
+
+  const skeletonHtml = getSkeletonHtml(page)
+  console.log("skeletonHtml", skeletonHtml)
+
+  //   const content = await getScriptContent()
+  await page.addScriptTag({url: 'https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js', content: getDomByScript })
+  //   await sleep(defer)
+  //   await this.page.evaluate(async (options) => {
+  //     const { genSkeleton } = Skeleton
+  //     genSkeleton(options)
+  //   }, options)
+  //   console.log("file: index.js ~ line 33 ~ content", content)
   await page.screenshot({
-    path: new Date().getTime()+'.png',
+    path: new Date().getTime() + '.png',
     fullPage: true
   });
-  const skeletonHtml = getSkeletonHtml(page)
-  console.log("skeletonHtml",skeletonHtml)
-
-//   const content = await getScriptContent()
-//   await this.page.addScriptTag({ content })
-//   await sleep(defer)
-//   await this.page.evaluate(async (options) => {
-//     const { genSkeleton } = Skeleton
-//     genSkeleton(options)
-//   }, options)
-//   console.log("file: index.js ~ line 33 ~ content", content)
-
   await browser.close();
 }
 openPage()
--- 自动生成页面骨架/util.js
+++ 自动生成页面骨架/util.js
@@ -2,11 +2,14 @@
  * @Author: yuyongxing
  * @Date: 2021-11-11 18:56:42
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-11-11 19:12:07
+ * @LastEditTime: 2021-11-11 23:49:50
  * @Description: 
  */
 function getSkeletonHtml(page) {
     let html = ""
     return html
+}
+function getDomByScript(){
+  return  document.getElementsByTagName("img")[0].style.display="none"
 }
 module.exports.getSkeletonHtml = getSkeletonHtml
