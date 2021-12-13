7d558e35d0b24071372d8b2d63a58edbebe6b5aa
yuwuwu <yyxpublic@163.com>
2021-12-13 23:14:33 +0800

    generator

--- 串行异步操作/index.js
+++ 串行异步操作/index.js
@@ -2,42 +2,44 @@
  * @Author: yuyongxing
  * @Date: 2021-12-07 21:54:35
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-12-13 20:17:47
+ * @LastEditTime: 2021-12-13 22:59:55
  * @Description: 
  */
 
 
-let callback1 = () => {
-    setTimeout(() => {
-        console.log("callback1")
-        callback2()
-    }, 1000);
-}
-let callback2 = () => {
-    setTimeout(() => {
-        console.log("callback2")
-    }, 500);
-}
-// callback1()
+// let callback1 = (callback) => {
+//     setTimeout(() => {
+//         console.log("callback1")
+//         callback()
+//     }, 1000);
+// }
+// let callback2 = () => {
+//     setTimeout(() => {
+//         console.log("callback2")
+//     }, 500);
+// }
+// callback1(callback2)
 
-let promise1 = (name) => new Promise((resolve) => {
-    setTimeout(() => {
-        console.log("promise1")
-        resolve()
-    }, 2000);
-})
-let promise2 = (name) => new Promise((resolve) => {
-    setTimeout(() => {
-        console.log("promise2")
-        resolve()
-    }, 1500);
-})
+// let promise1 = () => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("promise1")
+//         resolve()
+//     }, 2000);
+// })
+// let promise2 = () => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("promise2")
+//         resolve()
+//     }, 1500);
+// })
 
-promise1().then(()=>{
-    return promise2()
-}).then(()=>{
-    
-})
+// promise1()
+//     .then(() => {
+//         return promise2()
+//     })
+//     .then(() => {
+
+//     })
 
 
 let promise = (name) => new Promise((resolve) => {
@@ -46,36 +48,34 @@ let promise = (name) => new Promise((resolve) => {
         resolve()
     }, 1000);
 })
+let g1 = function(){
+    setTimeout(() => {
+        console.log(12)
+    }, 2000);
+}
 let generator = function* () {
-    yield promise("generator1")
+    yield g1()
     yield promise("generator2")
 }
-console.log(generator())
-generator().next()
-// generator().next()
-let co = generator => {
-    if (it = generator.next().value) {
-        it.then(res => {
-            co(generator)
-        })
-    } else {
-        return
-    }
-}
+let it = generator()
+it.next()
+it.next()
 
-let asyncAwait = async () => {
-    await promise("asyncAwait1")
-    await promise("asyncAwait2")
-    await promise("asyncAwait3")
-}
-asyncAwait()
 
-let event = (name) => {
-    event => {
-        setTimeout(() => {
-            console.log(name)
-            event.emit('end')
-        }, 1000);
-        return event
-    }
-}
+
+// let asyncAwait = async () => {
+//     await promise("asyncAwait1")
+//     await promise("asyncAwait2")
+//     await promise("asyncAwait3")
+// }
+// asyncAwait()
+
+// let event = (name) => {
+//     event => {
+//         setTimeout(() => {
+//             console.log(name)
+//             event.emit('end')
+//         }, 1000);
+//         return event
+//     }
+// }
