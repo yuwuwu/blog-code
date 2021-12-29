4e05f24f6d0dda269c524a17d21b6d3610e2e74a
yuyongxing <yyxpublic@163.com>
2021-12-29 18:17:59 +0800

    提交代码

--- 下载网页所有图片/index.js
+++ 下载网页所有图片/index.js
@@ -2,7 +2,7 @@
  * @Author: yuyongxing
  * @Date: 2021-11-24 19:39:28
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-12-29 10:35:15
+ * @LastEditTime: 2021-12-29 18:03:04
  * @Description: 
  */
 const puppeteer = require('puppeteer');
@@ -14,24 +14,44 @@ async function openPage() {
         device: "iPhone 6"
     }
     // 初始化puppeteer
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:true});
     // 打开页面
     const page = await browser.newPage();
     // 设定打开页面的设备
-    await page.emulate(puppeteer.devices[option.device])
+    // await page.emulate(puppeteer.devices[option.device])
+    await page.setViewport({width: 1190, height: 8000});
     // 打开的页面地址
     await page.goto(option.url, { waitUntil: "networkidle0" });
+    await page.evaluate(() => { 
+        window.scrollTo(0,3000)
+        setTimeout(()=>{
+           return Promise.resolve(window.scrollTo(0,document.body.scrollHeight))
+        },3000)
+        
+    })
 
+    const arr= await page.evaluate(()=>{
+        let images = document.querySelectorAll('img')
+        let srcs = []
+        for(let i=0;i<images.length;i++){
+            console.log(images[i])
+           srcs.push(images[i].src)
+        }
+        return srcs
+        console.log(images)
+        return new Promise((resolve,reject)=>{
+           for(let i in images){
 
-    const skeletonHtml = getSkeletonHtml(page)
+           }
+        })
+        
+    })
+    console.log(arr)
+
+  
     
-    console.log(skeletonHtml)
-    //   await sleep(defer)
-    // await this.page.evaluate(async (options) => {
-    //   const { genSkeleton } = Skeleton
-    //   genSkeleton(options)
-    // }, options)
-    //   console.log("file: index.js ~ line 33 ~ content", content)
+   
+
     await page.screenshot({
         // path: new Date().getTime() + '.png',
         path: 'test.png',
