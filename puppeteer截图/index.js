07d4fdc01aeba0efee81a6ce63ae16c1ddf49f05
yuwuwu <yyxpublic@163.com>
2022-01-13 00:05:41 +0800

    提交代码

--- puppeteer截图/index.js
+++ puppeteer截图/index.js
@@ -2,19 +2,22 @@
  * @Author: yuyongxing
  * @Date: 2022-01-10 11:33:26
  * @LastEditors: yuyongxing
- * @LastEditTime: 2022-01-12 18:10:55
+ * @LastEditTime: 2022-01-13 00:04:37
  * @Description: 
  */
+const express = require('express');
+const app = express()
 const puppeteer = require("puppeteer");
-const waitTime = (n) => new Promise((r) => setTimeout(r, n));
- 
-const main = async () => {
+
+
+const getImg = async (opt) => {
+  console.log(opt,"opt")
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const className = ".share-box";
   await page.goto("https://yqft-api.test.hi-cloud.net/activity-new/common/testShare.html");
   await page.setViewport({
-    width: 750,
+    width: opt.width,
     height: 750,
   });
   await waitTime(2000);
@@ -23,4 +26,17 @@ const main = async () => {
   await item.screenshot({ path: "item.png" });
   await browser.close();
 };
-main()
+
+
+const waitTime = (n) => new Promise((r) => setTimeout(r, n));
+app.use(express.json())
+app.post('/api/getShareImg', (req, res) => {
+  console.log(req.query,req.params,req.body, "参数")
+  getImg(req.body)
+
+});
+const server = app.listen(7000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('app start listening at http://%s:%s', host, port);
+});
