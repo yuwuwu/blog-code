28633f3ed67e837540305e30197732c40150eb4e
yuyongxing <yyxpublic@163.com>
2022-01-12 18:29:20 +0800

    修改测试路径

--- puppeteer截图/index.js
+++ puppeteer截图/index.js
@@ -2,7 +2,7 @@
  * @Author: yuyongxing
  * @Date: 2022-01-10 11:33:26
  * @LastEditors: yuyongxing
- * @LastEditTime: 2022-01-10 13:47:16
+ * @LastEditTime: 2022-01-12 18:10:55
  * @Description: 
  */
 const puppeteer = require("puppeteer");
@@ -11,8 +11,8 @@ const waitTime = (n) => new Promise((r) => setTimeout(r, n));
 const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const className = ".bili-banner";
-  await page.goto("https://www.bilibili.com/");
+  const className = ".share-box";
+  await page.goto("https://yqft-api.test.hi-cloud.net/activity-new/common/testShare.html");
   await page.setViewport({
     width: 750,
     height: 750,
