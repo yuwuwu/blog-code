1003df809c5daef92c75d911832041905532107a
yuyongxing <yyxpublic@163.com>
2022-01-13 18:23:40 +0800

    完善puppeteer

--- puppeteer截图/index.js
+++ puppeteer截图/index.js
@@ -2,40 +2,46 @@
  * @Author: yuyongxing
  * @Date: 2022-01-10 11:33:26
  * @LastEditors: yuyongxing
- * @LastEditTime: 2022-01-13 00:04:37
+ * @LastEditTime: 2022-01-13 18:14:02
  * @Description: 
  */
 const express = require('express');
 const app = express()
-const puppeteer = require("puppeteer");
+const getImg =require('./puppeteer.js')
 
 
-const getImg = async (opt) => {
-  console.log(opt,"opt")
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const className = ".share-box";
-  await page.goto("https://yqft-api.test.hi-cloud.net/activity-new/common/testShare.html");
-  await page.setViewport({
-    width: opt.width,
-    height: 750,
-  });
-  await waitTime(2000);
-  const item = await page.$(className);
-  // 不加path 返回buffer
-  await item.screenshot({ path: "item.png" });
-  await browser.close();
-};
 
 
-const waitTime = (n) => new Promise((r) => setTimeout(r, n));
 app.use(express.json())
 app.post('/api/getShareImg', (req, res) => {
-  console.log(req.query,req.params,req.body, "参数")
-  getImg(req.body)
+  let params = req.body
+  let errorMap = {
+    width: "海报宽度",
+    height: "海报高度",
+    url: "生成海报页面地址",
+    ele: "截取的元素",
+  }
+  for (let i in errorMap) {
+    if (!params[i]) {
+      res.json({
+        code: 0,
+        msg: `${errorMap[i]}必传`
+      })
+      return
+    }
+  }
+
+
+  getImg(req.body).then(file => {
+    res.json({
+      code: 1,
+      data: file,
+      msg: ""
+    })
+  })
 
 });
-const server = app.listen(7000, () => {
+const server = app.listen(7000, '0.0.0.0', () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('app start listening at http://%s:%s', host, port);
---
+++ puppeteer截图/puppeteer.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: yuyongxing
+ * @Date: 2022-01-13 18:07:56
+ * @LastEditors: yuyongxing
+ * @LastEditTime: 2022-01-13 18:22:47
+ * @Description: 
+ */
+const puppeteer = require("puppeteer");
+const waitTime = (n) => new Promise((r) => setTimeout(r, n));
+module.exports = async (opt) => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '–disable-gpu',
+        '–disable-dev-shm-usage',
+        '–disable-setuid-sandbox',
+        '–no-first-run',
+        '–no-sandbox',
+        '–no-zygote',
+        '–single-process'
+      ]
+    });
+    const page = await browser.newPage();
+    await page.goto(opt.url);
+    await page.setViewport({
+      width: opt.width,
+      height: opt.height,
+    });
+    await waitTime(opt.waitTime||500);
+    const ele = await page.$(opt.ele);
+    const base64 = await ele.screenshot({
+      fullPage: false,
+      omitBackground: true,
+      encoding: 'base64'
+    });
+    await browser.close();
+    return base64
+  };
