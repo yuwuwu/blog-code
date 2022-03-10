fb5c09f7f527e8148d265e9f97fbf6ded399d45a
yuyongxing <yyxpublic@163.com>
2022-03-10 19:02:53 +0800

    修改配置文件

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -3,46 +3,59 @@
  * @Author: yuyongxing
  * @Date: 2022-03-09 11:46:31
  * @LastEditors: yuyongxing
- * @LastEditTime: 2022-03-09 18:27:47
+ * @LastEditTime: 2022-03-10 18:25:47
  * @Description: cli入口
  */
-const fs = require("fs")
-const path = require("path")
-const chalk = require('chalk');
-const program = require('commander');
-const inquirer = require('inquirer');
-// chalk.blue('Hello world!')
-console.log(chalk.green("正在创建..."), process.argv)
-console.log(chalk.green(process.cwd(), "当前路径"))
+const fs = require("fs");
+const version = require('../package.json').version
+const path = require("path");
+const chalk = require("chalk");
+const program = require("commander");
+const inquirer = require("inquirer");
+const rimraf = require("rimraf");
+const download = require("download-git-repo");
+const ora = require("ora");
 
-program.version('0.0.1')
-    .parse(process.argv);
 
+program.version(version)
+       .usage()
+        parse(process.argv);
+
+const dir = path.resolve(process.cwd(), program.args[0]);
 console.log(program.args);
 if (fs.existsSync(program.args[0])) {
-    console.log(chalk.red(program.args[0] + "文件夹已存在"))
-    isRemoveDir()
+    console.log(chalk.red(program.args[0] + "文件夹已存在"));
+    isRemoveDir();
 } else {
-    fs.mkdirSync(path.resolve(process.cwd(), program.args[0]))
-    choiceTemplate()
+    fs.mkdirSync(dir);
+    choiceTemplate();
 }
 
 function choiceTemplate() {
     inquirer
-        .prompt([{
-            type: 'list',
-            name: 'choice',
-            message: '选择要使用的模板:',
-            default: 0,
-            choices: [
-                { value: "https://github.com/yuwuwu/vue-mobile-template", name: 'vue2+vant移动端模板' },
-                { value: "https://github.com/yuwuwu/vue-pc-template", name: 'vue2+element后台管理模板' }
-            ]
-        }])
-        .then(answers => {
-            console.log('answers', answers)
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "选择要使用的模板:",
+                default: 0,
+                choices: [
+                    {
+                        value: "github:yuwuwu/vue-mobile-template",
+                        name: "vue2+vant移动端模板",
+                    },
+                    {
+                        value: "github:yuwuwu/vue-pc-template",
+                        name: "vue2+element后台管理模板",
+                    },
+                ],
+            },
+        ])
+        .then((answers) => {
+            console.log("answers", answers);
+            downloadByGit(answers.choice);
         })
-        .catch(error => {
+        .catch((error) => {
             if (error.isTtyError) {
                 // Prompt couldn't be rendered in the current environment
             } else {
@@ -50,17 +63,42 @@ function choiceTemplate() {
             }
         });
 }
-function downloadByGit() {
-
-}
-function isRemoveDir() {
-    inquirer.prompt([{
-        type: 'confirm',
-        message: '是否覆盖原文件夹？',
-        name: 'ok'
-    }]).then(answers => {
-        if (answers.ok) {
-           console.log(123)
+function downloadByGit(url) {
+    console.log(url)
+    const loading = ora("downloading").start()
+    download(url, dir, { clone: false }, function (err) {
+        loading.stop()
+        if (err) {
+            console.log(chalk.red(err));
+            process.exit(1)
         }
-    })
+        console.log(chalk.green('success!'));
+    });
+}
+
+/**
+ * @Author: yuyongxing
+ * @param {*}
+ * @return {*}
+ * @Date: 2022-03-10 15:37:25
+ * @LastEditors: yuyongxing
+ * @LastEditTime: Do not edit
+ * @Description:
+ */
+function isRemoveDir() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "是否覆盖原文件夹？",
+                name: "ok",
+            },
+        ])
+        .then((answers) => {
+            if (answers.ok) {
+                rimraf.sync(dir);
+                fs.mkdirSync(dir);
+                choiceTemplate()
+            }
+        });
 }
