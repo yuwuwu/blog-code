ebfc9a505b2f7290415c0b2afe27643a4ba864f6
yuyongxing <yyxpublic@163.com>
2022-04-07 14:13:47 +0800

    1.删除bugfix配置文件个人信息 2.修改git过滤文件

---
+++ bugfix脚本/config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: yuyongxing
+ * @Date: 2022-02-25 16:15:28
+ * @LastEditors: yuyongxing
+ * @LastEditTime: 2022-04-07 14:12:46
+ * @Description: 邮箱动态项
+ */
+module.exports = {
+    SendKey:'SCT125580THiNYywv3IoD8eiJo7DESFHIr',
+    users: [
+        {
+            aid: "",
+            uuid:"",
+            cookie: ``
+         },
+
+    ]
+
+}
