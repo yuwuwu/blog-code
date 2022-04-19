366c847660ab4e815840f450dab8b3f8063d9e60
yuyongxing <yyxpublic@163.com>
2022-04-19 18:27:30 +0800

    提交配置文件

---
+++ node搭建消息推送/send-wx-message/utils/wxConfig.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: yuyongxing
+ * @Date: 2022-04-08 10:40:07
+ * @LastEditors: yuyongxing
+ * @LastEditTime: 2022-04-19 18:25:34
+ * @Description: 
+ */
+module.exports = {
+    appid:"",
+    secret:"",
+    token:"",
+    grant_type: 'client_credential'
+}
