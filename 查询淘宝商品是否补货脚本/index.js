cd04118ba29e758f1496a7d69d918429df6ccd1c
yuyongxing <yyxpublic@163.com>
2024-06-20 17:37:52 +0800

    update:增加git过滤，将个人敏感信息过滤

--- 查询淘宝商品是否补货脚本/index.js
+++ 查询淘宝商品是否补货脚本/index.js
@@ -2,8 +2,8 @@
  * @Author: yuwuwu
  * @Date: 2024-06-17 17:27:34
  * @LastEditors: yuwuwu
- * @LastEditTime: 2024-06-18 21:49:20
- * @FilePath: \查询淘宝商品是否补货脚本\index.js
+ * @LastEditTime: 2024-06-20 17:31:22
+ * @FilePath: /markdown-code/查询淘宝商品是否补货脚本/index.js
  * @Description:
  */
 const getTbGoods = require("./src/getGoodsByTb.js");
@@ -16,7 +16,7 @@ const tb_goods_url = [
   { url: "https://detail.tmall.com/item.htm?id=702916527846", title: "R50" },
   { url: "https://detail.tmall.com/item.htm?id=623306082845", title: "R5" },
 ];
-const wx_sku_list = ['3027','3127','3021']
+const wx_sku_list = ['3027','3127','3021','3132','3133','3130','3131','3162','3249']
 
 const start = async () => {
   // const tb_goods = await getTbGoods(tb_goods_url);
